f4214ca9a005155827842d9a37fb8c242f85d562
Caitlin Ross <rossc3@rpi.edu>
2016-04-20 16:40:54 -0400

    finished up parallel coords for all sim data

--- src/allpc.js
+++ src/allpc.js
@@ -4,21 +4,21 @@
 // TODO change to pull in ending simulation data for all runs
 // expects format
 // PE, GVT interval, batch, num KPs, runtime, % efficiency, net events, roll backs, ...
-d3.csv('data/alldata-batch.csv', function(data) {
+d3.csv('testdata.csv', function(data) {
     var colorgen = d3.scale.ordinal()
         .range(["#a6cee3","#1f78b4","#b2df8a","#33a02c",
             "#fb9a99","#e31a1c","#fdbf6f","#ff7f00",
             "#cab2d6","#6a3d9a","#ffff99","#b15928"]);
 
-    var color = function(d) { console.log(d); return colorgen(d.batch); };
+    var color = function(d) { return colorgen(d.batch); };
 
-    var parcoords = d3.parcoords()("#batch")
+    var parcoords = d3.parcoords()("#pc")
         .data(data)
      //   .hideAxis(["name"])
         .color(color)
         .alpha(0.25)
         .composite("darker")
-        .margin({ top: 54, left: 150, bottom: 12, right: 0 })
+        .margin({ top: 15, left: 0, bottom: 15, right: 0 })
         .mode("queue")
         .render()
         .reorderable()
