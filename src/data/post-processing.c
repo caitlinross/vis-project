16e36453d221f18be3dccf89cd8df3d05e49bbaa
Caitlin Ross <rossc3@rpi.edu>
2016-05-02 14:11:50 -0400

    fixed typo in post-processing and event log output

--- src/data/post-processing.c
+++ src/data/post-processing.c
@@ -241,8 +241,8 @@ for(f=0; f<num_pes; f++)
 	}
 
 	printf("Printing output LP and PE data\n");
-	fprintf(output_file_lp,"GVT-Bin,");
-	fprintf(output_file_pe,"GVT-Bin,");
+	fprintf(output_file_lp,"gvt,");
+	fprintf(output_file_pe,"gvt,");
 	for(i=0;i<num_lps-1;i++)
 	{
 		fprintf(output_file_lp,"LP_%d,",i); 
